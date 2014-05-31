e8996565e868800a8523776d0bb7e86c74d5bfd1
Tianqi(Jack) Jin <jackjin77817@gmail.com>
2014-05-30 22:53:26 -0400

    complemet allSubsetOfSet

--- cs_allSubsetOfSet/src/cs_allSubsetOfSet/Main.java
+++ cs_allSubsetOfSet/src/cs_allSubsetOfSet/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args){
-		ArrayList list = new ArrayList<>();
+		ArrayList list = new ArrayList();
 		for(int i = 0; i < 3; i++)
 			list.add(i);
 		findAllSubset(list);
@@ -12,14 +12,35 @@ public class Main {
 		
 	}
 	public static void findAllSubset(ArrayList list){
-		int i = 3;
-		StringBuilder sb = new StringBuilder();
-		for(int j = 0; j < 3; j++){
-			sb.append((i | 0) == 0? 0 : 1);
-			i >>= 1;
-			//System.out.println(tempInt);
+		int length = (int)Math.pow(2, list.size());
+		ArrayList<StringBuilder> al = new ArrayList<StringBuilder>();
+		for(int i = 0; i < length; i++){
+			String temp = Integer.toBinaryString(i);
+			//System.out.println(temp);
+			StringBuilder sb = new StringBuilder();
+			int distinct = list.size() - temp.length();
+			int distinctTemp = distinct;
+			for(int j = 0; j < list.size(); j++){
+				if(temp.length() == list.size())
+					sb.append(temp.charAt(j));
+				else{
+					
+					if(distinct > 0)
+						sb.append('0');
+					else
+						sb.append(temp.charAt(j-distinctTemp));
+					distinct--;
+				}
+			}
+			al.add(sb);
 		}
-		System.out.println(sb);
+		for(int i = 0; i < al.size(); i++){
+			for(int j = 0; j < al.get(i).length(); j++){
+				if(al.get(i).charAt(j) == '1')
+					System.out.print(list.get(j));
+			}
+			System.out.println();
+		}
 		
 	}
 }
