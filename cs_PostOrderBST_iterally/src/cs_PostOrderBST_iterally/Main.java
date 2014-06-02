8a45a34479cf1158b460c8378d82eb9d2ebc9ba7
Tianqi(Jack) Jin <jackjin77817@gmail.com>
2014-06-02 17:27:11 -0400

    minor modification

--- cs_PostOrderBST_iterally/src/cs_PostOrderBST_iterally/Main.java
+++ cs_PostOrderBST_iterally/src/cs_PostOrderBST_iterally/Main.java
@@ -5,9 +5,9 @@ public class Main {
 		Tree tree = new Tree();
 		tree.add(5);
 		tree.add(3);
-		tree.add(2);
-		tree.add(1);
-		tree.add(6);
+//		tree.add(2);
+//		tree.add(1);
+//		tree.add(6);
 		tree.add(7);
 		literallyPostOrder(tree.root);
 	}
